e31fcb9588987c469196078a6bf97953ac6918a4
Geback <prosolena13@gmail.com>
2025-02-18 19:39:47 +0500

    Add files via upload

---
+++ 8 ферзей/8 pherzey/8 pherzey/8 pherzey.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+int n = 8;
+int arr[8][8] = { 0 };
+
+void show() {
+	for (int i = 0; i < n; i++) {
+		
+		for (int j = 0; j < n; j++) {
+			cout << (arr[i][j] ? "# " : "o ");
+		}
+		cout << endl;
+	}
+	cout << "--------------------" << endl;
+}
+
+int chek(int c, int r)   
+{                      
+	for (int i = 0; i < c; ++i)
+	{
+		// Проверка горизонтали
+		if (arr[i][r]) return 0;
+		// Проверка диагонали
+		if ((r - c + i >= 0) && (arr[i][r - c + i])) return 0;
+		if ((r + c - i < 8) && (arr[i][r + c - i])) return 0;
+	}
+	return 1;
+}
+
+int set(int line)
+{
+	if (line == 8)
+	{
+		show();
+		return 1;
+	}
+	
+	for (int j = 0; j < n; j++) {
+		if (chek(line, j)) 
+		{
+			arr[line][j] = 1;
+			//show();
+			if (set(line + 1)) return 1;
+			arr[line][j] = 0;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	set(0);
+	return 0;
+}
---
+++ Ханойская башня/Han-Hanych/Han-Hanych/Han-Hanych.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+int c;
+
+void Pi(int n)
+{
+    while (n > 0)
+    {
+        cout << "*";
+        n--;
+    }
+    cout << endl;
+}
+
+void Han(int n, int start, int point,int temp)
+{
+    if (n != 0)
+    {
+        Han(n - 1, start, temp, point);
+        Pi(n);
+        cout << start << "=>" << point << endl;
+        Han(n - 1, temp, point, start);
+    }
+    return;
+}
+
+
+int main()
+{
+    cin >> c;
+    cout << endl;
+    while (c <= 0)
+    {
+        cout << "VVedi pologitelnoe chiclo:";
+        cin >> c;
+    }
+    Han(c, 1, 3, 2);
+    return 0;
+}
+
+
+
