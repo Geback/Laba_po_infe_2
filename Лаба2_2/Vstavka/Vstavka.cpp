b6787f87829fb4140e3d81c13e6d7f22448e97ec
Geback <prosolena13@gmail.com>
2025-02-18 19:48:23 +0500

    Add files via upload

---
+++ Лаба2_2/Vstavka/Vstavka.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int n = 10;
+	int m[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		m[i] = rand() % 1001 - 500;
+	}
+	cout << "Begin:";
+	for (int i = 0; i < n; i++)
+	{
+		cout << m[i] << " ";
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		
+		bool f = true;
+		for (int j = i; (j >= 0) && f; j--)
+		{
+			if (m[j] < m[j - 1])
+			{
+				int r = m[j];
+				m[j] = m[j - 1];
+				m[j - 1] = r;
+			}
+			else {
+				f = false;
+			}
+
+			
+		}
+		cout << endl;
+		for (int i = 0; i < n; i++)
+		{
+			cout << m[i] << " ";
+		}
+	}
+	cout << endl << "End:";
+	for (int i = 0; i < n; i++)
+	{
+		cout << m[i] << " ";
+	}
+	
+	return 0;
+}
