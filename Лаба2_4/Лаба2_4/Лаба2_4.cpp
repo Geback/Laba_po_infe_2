ad9d244850a52fb375c853b74a97ee31ea3c245c
Geback <prosolena13@gmail.com>
2025-02-20 15:16:53 +0500

    Add files via upload

---
+++ Лаба2_4/Лаба2_4/Лаба2_4.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int n;   //это К
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    const int S = 6;
+    cin >> n;
+    int a[S] = { 5,3,2,1, 9, 4 };
+    int o[S + 3];
+    int k = 0;
+
+    
+    for (int i = 0; i < S; i++) {
+        cout << a[i] << " ";
+    }  
+    cout << "Изначальный массив" << endl;
+
+    for (int i = n; i >= 0; i--) {
+        
+        cout << a[i] << " ";
+        o[k] = a[i];
+        k++;
+    }
+
+    for (int i = S-1; (i > n-1); i--) {
+
+        cout << a[i] << " ";
+        o[k] = a[i];
+        k++;
+    }
+    cout << "Массив от К до К-1 (влево)";
+    o[k] = a[0];
+    k++;
+    o[k] = a[S-1];
+    k++;
+
+    cout << endl;
+    for (int i = 0; i < S + 3; i++) {
+        cout << o[i] << " ";
+    }
+
+    cout << "Выведенный массив + первый и последний элементы" << endl;
+    for (int i = n; i < S+3; i++) {
+
+        cout << o[i] << " ";
+       
+    }
+    for (int i = 0; i < n; i++) {
+
+        cout << o[i] << " ";
+    }
+    cout << "Выведенный массив от К до К+1 (вправо)";
+    return 0;
+}
+
---
+++ Лаба2_5/Лаба2_5/Лаба2_5.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+int n = 5;
+bool F = true;
+//{5 52 46 73 47 43 74 1 54 61 17 81 12 8 29 76 2 79 84 55 20 7 83 13 58}
+// 54 49 21 79 4 34 81 33 50 49 49 66 64 64 73 60 28 32 2 31 63 8 2 17 5
+int main()
+{
+	int ki = 0;
+	int** arr = new int* [n]; 
+
+	for (int k = 0; k < n; k++) { 
+		arr[k] = new int[n];
+	}
+
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+		
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	int pastm = -99;
+	for (int j = 0;j<n;j++){
+		
+		/*for (int i = 0; i < n; i++) {  //Выводит столбцы 
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;*/
+
+		int min = 999999;
+		for (int i = 0; i < n; i++) {
+
+			if ((min > arr[i][j]) && (pastm < arr[i][j])) {
+				min = arr[i][j];
+				ki = i;
+				//cout << ".";
+			}
+		}
+		if (pastm == 999999) {
+			F = false;
+		}
+
+		//cout << "min:" << min << "   " << "kj:" << ki <<" << "<<pastm;
+		//cout << endl;
+		pastm = min;
+
+		int b = arr[n - j-1][j];
+		arr[n - j-1][j] = arr[ki][j];
+		arr[ki][j] = b;
+	}
+	cout << endl;  
+
+	if (F) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << arr[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else {
+		cout << "Error!!! It's impossible to arrange elements on the secondary diagonal so that they increase.";
+	}
+	return 0;
+}
