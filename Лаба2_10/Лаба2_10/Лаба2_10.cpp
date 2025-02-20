0a486e4bbb2990c74f99e7a81e5f838c16da0ce9
Geback <prosolena13@gmail.com>
2025-02-20 15:10:32 +0500

    Add files via upload

---
+++ Лаба2_10/Лаба2_10/Лаба2_10.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+// kdufbs fdshdbfus fsfd fksfbsifj fgdfthd
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n = 5;
+	cout << "Введите " <<n<<" строк:";
+
+	string* mas = new string[n];
+	for (int i = 0; i < n; ++i)
+		cin >> mas[i];
+	cout << endl;
+
+	int shorts = 0;
+	for (int i = 1; i < n; i++) {
+		if ((mas[i].length()) < (mas[shorts].length())) {
+			shorts = i;
+		}
+	}
+
+	string* mass = new string[n-1];
+	int j = 0;
+	for (int i = 0; i < n; ++i) {
+		if (i != shorts) {
+			mass[j++] = mas[i];
+		}
+	}
+	delete[] mas;
+	mas = nullptr;
+	
+	for (int i = 0; i < n-1; i++){
+		cout << mass[i] << " ";
+	}
+	delete[] mass;
+	mass = nullptr;
+
+	return 0;
+}
+
+
---
+++ Сортировка по 2 строке/Сортировка по 2 строке.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+int k, min;
+
+int main()
+{
+	const int n = 3;
+	int arr[n][n] = { 7, 8, 2, 5, 9, 2 ,8, 4, 3 };
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int min = 999999999;
+		for (int j = n - 1; j >= i; j--)
+		{
+			if (arr[1][j] < min)
+			{
+				k = j;
+				min = arr[1][j];
+			}
+
+		}
+		int r2 = arr[2][i];
+		arr[2][i] = arr[2][k];
+		arr[2][k] = r2;
+
+		int r1 = arr[1][i];
+		arr[1][i] = arr[1][k];
+		arr[1][k] = r1;
+
+		int r0 = arr[0][i];
+		arr[0][i] = arr[0][k];
+		arr[0][k] = r0;
+	}
+
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
