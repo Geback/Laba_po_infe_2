204eebf7d9390d971f20b3febd4268b9884057b5
Geback <prosolena13@gmail.com>
2025-04-02 05:26:45 +0300

    Add files via upload

---
+++ Part1_5/Методы поиска2/Методы поиска/Бинарный/Бинарный.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int binarySearch(const int arr[], int size, int t)
+{
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right){
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == t)
+            return mid;
+
+        if (arr[mid] < t){
+            left = mid + 1;
+        }else{
+            right = mid - 1;}
+    }
+    return -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int arr[] = { -18, -17, -11, -10, -6, 5, 9, 12, 15, 32, 35, 49 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int t;
+    cout << "Введите число которое хотите найти: ";
+    cin >> t;
+    int result = binarySearch(arr, size, t);
+
+    if (result != -1){
+        cout << "Элемент найден по индексу: " << result << endl;}
+    else{
+        cout << "Элемент не найден." << endl;}
+
+    return 0;
+}
---
+++ Part1_5/Методы поиска2/Методы поиска/Интерполяционный/Интерполяционный.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int interSearch(int* arr, int t, int s)
+{
+    int left = 0;
+    int right = s;
+
+    while (left <= right && t >= arr[left] && t <= arr[right])
+    {
+        if (left == right){
+            if (arr[left] == t) return left;
+            return -1;
+        }
+
+        int pos = left + ((double)(right - left) / (arr[right] - arr[left])) * (t - arr[left]);
+
+        if (arr[pos] == t) 
+            return pos;
+
+        if (arr[pos] < t){
+            left = pos + 1;
+        }
+        else{
+            right = pos - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+   
+    int arr[] = {10, 13, 16, 18, 19, 20, 21, 22, 24, 33, 35, 42, 47};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int t;
+    cout << "Введите число которое хотите найти: ";
+    cin >> t;
+
+    int ind = interSearch(arr, t, size - 1);
+
+    if (ind != -1){
+        cout << "Элемент " << t << " найден по индексу " << ind << endl;
+    }else{
+        cout << "Элемент " << t << " не найден" << endl;}
+    return 0;
+}
