97aba1b10b57a31b6676b40f1f2aa96fd0d867f5
Geback <prosolena13@gmail.com>
2025-04-02 05:29:07 +0300

    Add files via upload

---
+++ Part1_5/Поиски тире2/Поиски с тире в названии/Бойер Мур-Хорснул/Бойер Мур-Хорснул.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+void prepBadChar(char* oo, int pSize, int badChar[256]){
+    for (int i = 0; i < 256; i++)
+        badChar[i] = -1;
+
+    for (int i = 0; i < pSize; i++)
+        badChar[(int)oo[i]] = i;
+}
+
+void search(char* s, char* o, int sSize, int oSize)
+{
+    bool sheald = true;
+    int badChar[256];
+    prepBadChar(o, oSize, badChar);
+
+    int shift = 0;
+    while (shift <= (sSize - oSize))
+    {
+        int j = oSize - 1;
+
+        while (j >= 0 && o[j] == s[shift + j])
+            j--;
+
+        if (j < 0){
+            cout << "Образ начинается с " << shift << " индекса" << endl;
+            sheald = false;
+            shift += (shift + oSize < sSize) ? oSize - badChar[s[shift + oSize]] : 1;
+        }else{
+            if ((j - badChar[s[shift + j]]) > 1){
+                shift += j - badChar[s[shift + j]];
+            }else{
+                shift += 1;
+            }
+        }
+    }
+
+    if (sheald){
+        cout << "Образ не найден" << endl;
+    }
+}
+
+int main(){
+    setlocale(LC_ALL, "rus");
+    char stroka[] = "abccbfdsadkacaabcd";
+    char obraz[] = "aabc";
+
+    int sSize = (sizeof(stroka) / sizeof(stroka[0])) - 1;
+    int oSize = (sizeof(obraz) / sizeof(obraz[0])) - 1;
+    search(stroka, obraz, sSize, oSize);
+
+    return 0;
+}
---
+++ Part1_5/Поиски тире2/Поиски с тире в названии/Кнут Моррис-Пратт/Кнут Моррис-Пратт.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+
+void Prefix(const char* o, int oSize, int* pi)
+{
+    pi[0] = 0;
+
+    int k = 0;
+    for (int q = 1; q < oSize; ++q){
+        while (k > 0 && o[k] != o[q]){
+            k = pi[k - 1];
+        }
+
+        if (o[k] == o[q]){
+            k++;
+        }
+        pi[q] = k;
+    }
+}
+
+int KMP(const char* text, int tSize, const char* o, int oSize)
+{
+    if (oSize == 0) return 0;
+
+    int* pi = new int[oSize];
+    Prefix(o, oSize, pi);
+
+    int q = 0;
+
+    for (int i = 0; i < tSize; ++i){
+        while (q > 0 && o[q] != text[i]){
+            q = pi[q - 1]; 
+        }
+
+        if (o[q] == text[i]){
+            q++;
+        }
+
+        if (q == oSize){
+            delete[] pi;
+            return i - oSize + 1; 
+        }
+    }
+
+    delete[] pi;
+    return -1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const char* text = "ABABDABACDABABCABAB";
+    const char* obraz = "ABABCABAB";
+
+    int text_len = 0;
+    while (text[text_len] != '\0') text_len++;
+
+    int obraz_len = 0;
+    while (obraz[obraz_len] != '\0') obraz_len++;
+
+    int pos = KMP(text, text_len, obraz, obraz_len);
+
+    if (pos != -1){
+        cout << "Образ начинается с " << pos << " индекса" << endl;
+    }
+    else{
+        cout << "Образ не найден" << endl;
+    }
+
+    return 0;
+}
